59f5bfa657f2f61c50eb4f2acfff94b1c53c4eb6
Felipe Silva <felipe.ferreira1003@gmail.com>
2022-07-19 21:52:33 -0300

    create update and delete method to categories entity

--- src/app/controllers/CategoryController.js
+++ src/app/controllers/CategoryController.js
@@ -17,6 +17,37 @@ class CategoryController {
 
     response.json(category);
   }
+
+  async update(request, response) {
+    const { id } = request.params;
+    const { name } = request.body;
+
+    const categoryExists = await CategoriesRepository.findById(id);
+
+    if (!categoryExists) {
+      return response.status(404).json({ error: 'Category not found' });
+    }
+
+    if (!name) {
+      return response.status(400).json({ error: 'Name is required' });
+    }
+
+    const categoryByName = await CategoriesRepository.findByName(name);
+    if (categoryByName && categoryByName.id !== id) {
+      return response
+        .status(400)
+        .json({ error: 'This name is alreagy in use' });
+    }
+
+    const category = await CategoriesRepository.update(id, { name });
+    response.json(category);
+  }
+
+  async delete(request, response) {
+    const { id } = request.params;
+    await CategoriesRepository.delete(id);
+    response.sendStatus(204);
+  }
 }
 
 module.exports = new CategoryController();
--- src/routes.js
+++ src/routes.js
@@ -12,5 +12,7 @@ router.put('/contacts/:id', ContactController.update);
 
 router.get('/categories', CategoryController.index);
 router.post('/categories', CategoryController.store);
+router.put('/categories/:id', CategoryController.update);
+router.delete('/categories/:id', CategoryController.delete);
 
 module.exports = router;
