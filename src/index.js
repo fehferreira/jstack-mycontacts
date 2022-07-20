40872b324eca672990a2c5076fde06e9934d320d
Felipe Silva <felipe.ferreira1003@gmail.com>
2022-07-19 22:43:13 -0300

    create Error Handles to Internal Server Erros

--- src/index.js
+++ src/index.js
@@ -1,9 +1,18 @@
 const express = require('express');
+require('express-async-errors');
 const routes = require('./routes');
 
 const app = express();
 
 app.use(express.json());
 app.use(routes);
+
+// Error Handler(Middleware Express) -> Manipulador de erros
+// Error handler sempre depois da definição das rotas
+// eslint-disable-next-line no-unused-vars
+app.use((error, request, response, next) => {
+  console.log(error);
+  response.sendStatus(500);
+});
 
 app.listen(3000, () => console.log('Server started at http://localhost:3000'));
