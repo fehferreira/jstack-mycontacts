367c2ac0260ba7050224f5310826d7de4103c358
Felipe Silva <felipe.ferreira1003@gmail.com>
2022-07-19 22:20:22 -0300

    implement JOIN command in search querys for DB

--- src/app/repositories/ContactsRepository.js
+++ src/app/repositories/ContactsRepository.js
@@ -3,29 +3,45 @@ const db = require('../../database');
 class ContactsRepository {
   async findAll(orderBy = 'ASC') {
     const direction = orderBy.toUpperCase() === 'DESC' ? 'DESC' : 'ASC';
-    const rows = await db.query(
-      `SELECT * FROM contacts ORDER BY name ${direction}`
-    );
+    const rows = await db.query(`
+      SELECT contacts.*, categories.name AS category_name
+      FROM contacts
+      LEFT JOIN categories ON categories.id = contacts.category_id
+      ORDER BY contacts.name ${direction}
+    `);
     return rows;
   }
 
   async findById(id) {
-    const [row] = await db.query('SELECT * FROM contacts WHERE id = $1', [id]);
+    const [row] = await db.query(
+      `
+    SELECT contacts.*, categories.name AS category_name
+    FROM contacts
+    LEFT JOIN categories ON categories.id = contacts.category_id
+    WHERE contacts.id = $1
+    `,
+      [id]
+    );
     return row;
   }
 
   async findByEmail(email) {
-    const [row] = await db.query('SELECT * FROM contacts WHERE email = $1', [
-      email,
-    ]);
+    const [row] = await db.query(
+      `
+    SELECT contacts.*, categories.name AS category_name
+    FROM contacts
+    LEFT JOIN categories ON categories.id = contacts.category_id
+    WHERE contacts.email = $1
+    `,
+      [email]
+    );
     return row;
   }
 
   async create({ name, email, phone, category_id }) {
     const [row] = await db.query(
       `
-      INSERT INTO contacts(
-        name, email, phone, category_id)
+      INSERT INTO contacts(name, email, phone, category_id)
       VALUES($1, $2, $3, $4)
       RETURNING *
     `,
